a942b5a154db446095a43ffc7561d0b7e1139eb0
sabaka-chabaka <gamekimg360@gmail.com>
2026-03-10 01:11:56 +0400

    im tired

---
+++ Source/SSURVIVAL/Private/WeatherSubsystem.cpp
@@ -0,0 +1,109 @@
+// 2026 sabaka-chabaka
+
+#include "WeatherSubsystem.h"
+
+#include "Components/DirectionalLightComponent.h"
+
+void UWeatherSubsystem::Initialize(FSubsystemCollectionBase& Collection)
+{
+	Super::Initialize(Collection);
+	CurrentWeatherTable.Weather = EWeatherType::Clear;
+	CurrentWeatherTable.DayOfWeek = EDayOfWeek::Monday;
+	CurrentWeatherTable.Month = EMonthOfYear::July;
+	CurrentWeatherTable.Day = 1;
+	CurrentWeatherTable.Hours = 12;
+	CurrentWeatherTable.Minutes = 0;
+	CurrentWeatherTable.Seconds = 0;
+}
+
+void UWeatherSubsystem::Tick(float DeltaTime)
+{
+	CurrentWeatherTable.Seconds += DeltaTime * 24;
+
+	if (CurrentWeatherTable.Seconds >= 60)
+	{
+		CurrentWeatherTable.Seconds -= 60;
+		CurrentWeatherTable.Minutes++;
+	}
+
+	if (CurrentWeatherTable.Minutes >= 60)
+	{
+		CurrentWeatherTable.Minutes = 0;
+		CurrentWeatherTable.Hours++;
+	}
+
+	if (CurrentWeatherTable.Hours >= 24)
+	{
+		CurrentWeatherTable.Hours = 0;
+		CurrentWeatherTable.Day++;
+
+		CurrentWeatherTable.DayOfWeek =
+			static_cast<EDayOfWeek>(
+				(static_cast<uint8>(CurrentWeatherTable.DayOfWeek.GetValue()) + 1) % 7
+			);
+
+		UpdateMonth();
+	}
+}
+
+TStatId UWeatherSubsystem::GetStatId() const
+{
+	RETURN_QUICK_DECLARE_CYCLE_STAT(UWeatherSubsystem, STATGROUP_Tickables);
+}
+
+void UWeatherSubsystem::UpdateSunRotation()
+{
+	if (!SunLight)
+	{
+		return;
+	}
+
+	float TotalSeconds = (CurrentWeatherTable.Hours * 3600.0f) + (CurrentWeatherTable.Minutes * 60.0f) + CurrentWeatherTable.Seconds;
+	
+	float DayRatio = TotalSeconds / (24.0f * 3600.0f);
+	
+	float SunPitch = (DayRatio * 360.0f) + 90.0f;
+
+	FRotator NewRotation = FRotator(SunPitch, 0.0f, 0.0f);
+	SunLight->SetRelativeRotation(NewRotation);
+}
+
+void UWeatherSubsystem::UpdateMonth()
+{
+	int32 DaysInMonth = 30;
+
+	switch (CurrentWeatherTable.Month)
+	{
+	case EMonthOfYear::January:
+	case EMonthOfYear::March:
+	case EMonthOfYear::May:
+	case EMonthOfYear::July:
+	case EMonthOfYear::August:
+	case EMonthOfYear::October:
+	case EMonthOfYear::December:
+		DaysInMonth = 31;
+		break;
+
+	case EMonthOfYear::April:
+	case EMonthOfYear::June:
+	case EMonthOfYear::September:
+	case EMonthOfYear::November:
+		DaysInMonth = 30;
+		break;
+
+	case EMonthOfYear::February:
+		DaysInMonth = 28;
+		break;
+	default: break;
+	}
+
+	if (CurrentWeatherTable.Day > DaysInMonth)
+	{
+		CurrentWeatherTable.Day = 1;
+
+		CurrentWeatherTable.Month =
+			static_cast<EMonthOfYear>(
+				(static_cast<uint8>(CurrentWeatherTable.Month.GetValue()) + 1) % 12
+			);
+	}
+}
---
+++ Source/SSURVIVAL/Public/WeatherSubsystem.h
@@ -0,0 +1,80 @@
+// 2026 sabaka-chabaka
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Subsystems/WorldSubsystem.h"
+#include "Tickable.h"
+#include "WeatherSubsystem.generated.h"
+
+UENUM(BlueprintType)
+enum EWeatherType : uint8
+{
+	Clear,
+	Cloudy,
+	Rain,
+	Fog
+};
+
+USTRUCT(BlueprintType)
+struct FWeatherTable
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditAnywhere)
+	EWeatherType Weather;
+	
+	UPROPERTY(EditAnywhere)
+	TEnumAsByte<EDayOfWeek> DayOfWeek;
+	
+	UPROPERTY(EditAnywhere)
+	TEnumAsByte<EMonthOfYear> Month;
+	
+	UPROPERTY(EditAnywhere)
+	float Day;
+	
+	UPROPERTY(EditAnywhere)
+	float Hours;
+	
+	UPROPERTY(EditAnywhere)
+	float Minutes;
+	
+	UPROPERTY(EditAnywhere)
+	float Seconds;
+};
+
+UCLASS()
+class SSURVIVAL_API UWeatherSubsystem : public UWorldSubsystem, public FTickableGameObject
+{
+	GENERATED_BODY()
+	
+public:
+	
+	virtual void Initialize(FSubsystemCollectionBase& Collection) override;
+	
+	UFUNCTION(BlueprintCallable)
+	void ChangeWeatherTable(const FWeatherTable& NewTable) { CurrentWeatherTable = NewTable; }
+	
+	UFUNCTION(BlueprintCallable)
+	FWeatherTable GetWeather() const { return CurrentWeatherTable; }
+	
+	virtual void Tick(float DeltaTime) override;
+	virtual TStatId GetStatId() const override;
+	virtual bool IsTickable() const override { return true; };
+	
+private:
+	FWeatherTable CurrentWeatherTable;
+	
+protected:
+	UPROPERTY(EditAnywhere, Category="Weather")
+	class UVolumetricCloudComponent* CloudyComponent;
+	
+	UPROPERTY(EditAnywhere, Category="Weather")
+	class UExponentialHeightFogComponent* FogComponent;
+
+	UPROPERTY(EditAnywhere, Category="Weather")
+	class UDirectionalLightComponent* SunLight;
+	
+	void UpdateSunRotation();
+	void UpdateMonth();
+};
